673e97735996c57cc0c605cbc323af27c38a334d
Zach Lorimer <zlorimer@gmail.com>
2021-09-05 21:58:21 -0700

    Updated CSV files

--- src/postgres/conf/initdb.sql
+++ src/postgres/conf/initdb.sql
@@ -1,5 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE artists(uuid UUID DEFAULT uuid_generate_v4 () PRIMARY KEY, artist_name VARCHAR(150) NOT NULL);
 COPY artists(artist_name) FROM '/docker-entrypoint-initdb.d/artists.csv' DELIMITER ',' CSV HEADER;
-CREATE TABLE albums(uuid UUID DEFAULT uuid_generate_v4 () PRIMARY KEY, artist_name VARCHAR(150) NOT NULL, album_title VARCHAR(150) NOT NULL, release_date TIMESTAMP NOT NULL, price FLOAT NOT NULL);
+CREATE TABLE albums(uuid UUID DEFAULT uuid_generate_v4 () PRIMARY KEY, artist_name VARCHAR(150) NOT NULL, album_title VARCHAR(150) NOT NULL, release_date INTEGER NOT NULL, price FLOAT NOT NULL);
 COPY albums(artist_name, album_title, release_date, price) FROM '/docker-entrypoint-initdb.d/albums.csv' DELIMITER ',' CSV HEADER;
